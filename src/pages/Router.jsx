cf77836a2f2f1e9482e64700f419f3dbd0a9059a
HOSU HAN <tnghgks@naver.com>
2022-11-23 23:21:28 +0900

    🐛 버그 수정 (router base주소 변경)

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -5,9 +5,9 @@ import NotFound from "./NotFound/NotFound";
 const Router = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={"/"} element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
